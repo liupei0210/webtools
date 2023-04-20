7934a2efa9e10edf22972bed495e59ef1ff04c00
liupei <403664797@qq.com>
2023-04-20 15:43:07 +0800

    修改page

--- page/page.go
+++ page/page.go
@@ -2,21 +2,21 @@ package page
 
 import "gorm.io/gorm"
 
-func PageTemplate[T interface{}](pageNum, pageSize int, handler func() (*gorm.DB, error)) (*Page[T], error) {
+func PageTemplate[T interface{}](pageNum, pageSize int, handler func() (*gorm.DB, error)) (Page[T], error) {
 	var total int64
-	results := make([]T, 0)
+	results := make([]T, pageSize)
 	query, err := handler()
 	if err != nil {
-		return nil, err
+		return Page[T]{}, err
 	}
 	query.Count(&total)
 	if err = query.Limit(pageSize).Offset((pageNum - 1) * pageSize).Find(&results).Error; err != nil {
-		return nil, err
+		return Page[T]{}, err
 	}
-	return &Page[T]{
+	return Page[T]{
 		CurrentSize: len(results),
 		TotalSize:   total,
-		Content:     &results,
+		Content:     results,
 	}, nil
 }
 
@@ -25,7 +25,7 @@ type PageReq struct {
 	PageSize int `json:"page_size"`
 }
 type Page[T interface{}] struct {
-	Content     *[]T  `json:"content"`
+	Content     []T   `json:"content"`
 	CurrentSize int   `json:"current_size"`
 	TotalSize   int64 `json:"total_size"`
 }
