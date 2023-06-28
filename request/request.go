6e428283782e8280f3ebbe53fd75146d631f30fc
bravpei <403664797@qq.com>
2023-06-28 17:02:33 +0800

    参数不传指针

--- request/request.go
+++ request/request.go
@@ -8,7 +8,7 @@ import (
 )
 
 // ControllerTemplate is a template function for handling requests with JSON parameters.
-func ControllerTemplate[Params interface{}](ctx iris.Context, f func(p *Params) error) {
+func ControllerTemplate[Params interface{}](ctx iris.Context, f func(p Params) error) {
 	var params Params
 	if err := ctx.ReadJSON(&params); err != nil {
 		log.Errorf("failed to read JSON: %v", err)
@@ -21,7 +21,7 @@ func ControllerTemplate[Params interface{}](ctx iris.Context, f func(p *Params) 
 		_ = ctx.JSON(response.Fail(v.Errors.One()))
 		return
 	}
-	if err := f(&params); err != nil {
+	if err := f(params); err != nil {
 		log.Errorf("failed to handle request: %v", err)
 		_ = ctx.JSON(response.Fail(err.Error()))
 	}
