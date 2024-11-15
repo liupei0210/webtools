318ea8e879b77a30e77c0f37add423f413e4b2be
bravpei <bravpei@gmail.com>
2024-11-15 17:02:36 +0800

    http工具优化

--- external/pkg/utils/http.go
+++ external/pkg/utils/http.go
@@ -12,69 +12,22 @@ import (
 )
 
 type HttpClientWrapper struct {
-	client *http.Client
 	Domain string
 }
 
 func NewHttpClientWrapper(domain string) *HttpClientWrapper {
 	return &HttpClientWrapper{
 		Domain: domain,
-		client: http.DefaultClient,
 	}
 }
 func (wrapper *HttpClientWrapper) Get(api string, header map[string]string, queryParams url.Values, ctx ...context.Context) (*http.Response, error) {
-	return wrapper.request(http.MethodGet, api, header, queryParams, nil, ctx...)
+	return request(http.MethodGet, wrapper.Domain, api, header, queryParams, nil, ctx...)
 }
 func (wrapper *HttpClientWrapper) Post(api string, header map[string]string, queryParams url.Values, body []byte, ctx ...context.Context) (*http.Response, error) {
-	return wrapper.request(api, http.MethodPost, header, queryParams, body, ctx...)
+	return request(http.MethodPost, wrapper.Domain, api, header, queryParams, body, ctx...)
 }
 func (wrapper *HttpClientWrapper) Put(api string, header map[string]string, queryParams url.Values, body []byte, ctx ...context.Context) (*http.Response, error) {
-	return wrapper.request(api, http.MethodPut, header, queryParams, body, ctx...)
-}
-func (wrapper *HttpClientWrapper) request(api, method string, header map[string]string, queryParams url.Values, body []byte, ctx ...context.Context) (*http.Response, error) {
-	var reader io.Reader
-	if body != nil {
-		reader = bytes.NewReader(body)
-	}
-	var request *http.Request
-	var err error
-	if len(ctx) > 0 {
-		request, err = wrapper.assembleRequestWithContext(ctx[0], method, api, header, queryParams, reader)
-	} else {
-		request, err = wrapper.assembleRequest(method, api, header, queryParams, reader)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return wrapper.client.Do(request)
-}
-func (wrapper *HttpClientWrapper) assembleRequest(method string, api string, header map[string]string, queryParams url.Values, body io.Reader) (*http.Request, error) {
-	apiUrl := wrapper.Domain + api
-	if queryParams != nil {
-		apiUrl = apiUrl + "?" + queryParams.Encode()
-	}
-	request, err := http.NewRequest(method, apiUrl, body)
-	if err != nil {
-		return nil, err
-	}
-	for key, value := range header {
-		request.Header.Set(key, value)
-	}
-	return request, nil
-}
-func (wrapper *HttpClientWrapper) assembleRequestWithContext(ctx context.Context, method string, api string, header map[string]string, queryParams url.Values, body io.Reader) (*http.Request, error) {
-	apiUrl := wrapper.Domain + api
-	if queryParams != nil {
-		apiUrl = apiUrl + "?" + queryParams.Encode()
-	}
-	request, err := http.NewRequestWithContext(ctx, method, apiUrl, body)
-	if err != nil {
-		return nil, err
-	}
-	for key, value := range header {
-		request.Header.Set(key, value)
-	}
-	return request, nil
+	return request(http.MethodPut, wrapper.Domain, api, header, queryParams, body, ctx...)
 }
 func HandleResponse[T any](response *http.Response) (body T, err error) {
 	defer response.Body.Close()
@@ -88,4 +41,58 @@ func HandleResponse[T any](response *http.Response) (body T, err error) {
 	}
 	err = json.Unmarshal(bodyBytes, &body)
 	return
+}
+
+var assembleRequest = func(method, domain, api string, header map[string]string, queryParams url.Values, body io.Reader) (*http.Request, error) {
+	apiUrl := domain + api
+	if queryParams != nil {
+		apiUrl = apiUrl + "?" + queryParams.Encode()
+	}
+	request, err := http.NewRequest(method, apiUrl, body)
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+	return request, nil
+}
+var assembleRequestWithContext = func(ctx context.Context, method, domain, api string, header map[string]string, queryParams url.Values, body io.Reader) (*http.Request, error) {
+	apiUrl := domain + api
+	if queryParams != nil {
+		apiUrl = apiUrl + "?" + queryParams.Encode()
+	}
+	request, err := http.NewRequestWithContext(ctx, method, apiUrl, body)
+	if err != nil {
+		return nil, err
+	}
+	for key, value := range header {
+		request.Header.Set(key, value)
+	}
+	return request, nil
+}
+var request = func(method, domain, api string, header map[string]string, queryParams url.Values, body []byte, ctx ...context.Context) (*http.Response, error) {
+	var reader io.Reader
+	if body != nil {
+		reader = bytes.NewReader(body)
+	}
+	var req *http.Request
+	var err error
+	if len(ctx) > 0 {
+		req, err = assembleRequestWithContext(ctx[0], method, domain, api, header, queryParams, reader)
+	} else {
+		req, err = assembleRequest(method, domain, api, header, queryParams, reader)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return http.DefaultClient.Do(req)
+}
+
+func DoRequest[ResponseStruct any](method, domain, api string, header map[string]string, queryParams url.Values, body []byte, ctx ...context.Context) (resStruct ResponseStruct, err error) {
+	response, err := request(method, domain, api, header, queryParams, body, ctx...)
+	if err != nil {
+		return
+	}
+	return HandleResponse[ResponseStruct](response)
 }
